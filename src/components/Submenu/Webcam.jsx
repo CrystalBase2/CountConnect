2e47f12d37ee41957cc38f24bcb66f6d3ca6d601
CrystalBase2 <crystalbase02@gmail.com>
2023-12-06 00:29:36 +0800

    Changes in Webcam

--- src/components/Submenu/Webcam.jsx
+++ src/components/Submenu/Webcam.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { FaCalendarAlt } from "react-icons/fa";
-import { useState } from "react";
-
-import '../../css/Submenu.css';
+import '../../css/Webcam.css';
 
 
 function Webcam() {
@@ -11,10 +9,6 @@ function Webcam() {
   const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
   const day = currentDate.getDate().toString().padStart(2, '0');
   const formattedDate = `${year}-${month}-${day}`;
-
-  const [isActive, setIsActive] = useState(false);
-  const [selected, setIsSelected] = useState("Choose a bus route");
-
   return (
     <div className="submenu-container">
       <div className="submenu-content">
@@ -24,16 +18,8 @@ function Webcam() {
           <p className="submenu-date">{formattedDate}</p>
         </div>
       </div>
-      <span className="submenu-pagesub">Daily</span>
-      <span className="reports-submenu-subtitle">Welcome<b>, USER!</b></span>
-
-      </div>
-
-
-
+    </div>
 );
 }
-
-
 
 export default Webcam;
