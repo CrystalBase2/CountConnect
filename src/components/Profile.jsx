5307040e5391e2ad2c5a984504afbc524d28863d
CrystalBase2 <crystalbase02@gmail.com>
2023-12-05 17:06:34 +0800

    Modifications on Profile Page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { FaCalendarAlt } from "react-icons/fa";
-import { useState } from "react";
 import '../css/Home.css';
 
-function Home() {
+function Profile() {
   const currentDate = new Date();
   const year = currentDate.getFullYear();
   const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
   const day = currentDate.getDate().toString().padStart(2, '0');
   const formattedDate = `${year}-${month}-${day}`;
 
-  const [isActive, setIsActive] = useState(false);
-  const [selected, setIsSelected] = useState("Choose a bus route");
 
   return (
     <div className="home-container">
@@ -22,48 +19,6 @@ function Home() {
           <p className="home-date">{formattedDate}</p>
         </div>
       </div>
-        <span className="home-subtitle">Welcome<b>, USER!</b></span>
-
-      <div className="home-dropdown">
-      <div className="dropdown">
-        <div onClick={(e) => {setIsActive(!isActive);}} className="dropdown-btn">
-          {selected}
-          <p className={isActive ? "fas fa-caret-up" : "fas fa-caret-down"}/>
-        </div>  
-        <div className="dropdown-content" style={{ display: isActive ? "block" : "none" }}>
-        <div onClick={(e) => {
-              setIsSelected(e.target.textContent);
-              setIsActive(!isActive);
-            }}
-            className="dropdown-item"> Gaisano Mall - Alubijid
-          </div>
-          <div onClick={(e) => {
-              setIsSelected(e.target.textContent);
-              setIsActive(!isActive);
-            }}
-            className="dropdown-item"> Gaisano Mall - Laguindingan
-          </div>
-          <div onClick={(e) => {
-              setIsSelected(e.target.textContent);
-              setIsActive(!isActive);
-            }}
-            className="dropdown-item"> Gaisano Mall - Libertad
-          </div>
-          <div onClick={(e) => {
-              setIsSelected(e.target.textContent);
-              setIsActive(!isActive);
-            }}
-            className="dropdown-item"> Gaisano Mall - Tagoloan
-          </div>
-          <div onClick={(e) => {
-              setIsSelected(e.target.textContent);
-              setIsActive(!isActive);
-            }}
-            className="dropdown-item"> Gaisano Mall - Villanueva
-          </div>
-        </div>
-      </div>
-    </div>
     </div>
 
       
@@ -73,6 +28,6 @@ function Home() {
 
 
 
-export default Home;
+export default Profile;
 
 
