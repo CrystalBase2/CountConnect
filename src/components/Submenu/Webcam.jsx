d17c2196b6c5539e04a5a335910f335c7464c22b
CrystalBase2 <crystalbase02@gmail.com>
2023-12-07 00:19:59 +0800

    Added Buttons for Live Feed Monitoring Page

--- src/components/Submenu/Webcam.jsx
+++ src/components/Submenu/Webcam.jsx
@@ -4,7 +4,7 @@ import '../../css/Webcam.css';
 
 import BusIcon from '../../images/BusIcon.png';
 import TerminalIcon from '../../images/TerminalIcon.png';
-
+import { useNavigate } from "react-router-dom";
 
 function Webcam() {
   const currentDate = new Date();
@@ -12,6 +12,12 @@ function Webcam() {
   const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
   const day = currentDate.getDate().toString().padStart(2, '0');
   const formattedDate = `${year}-${month}-${day}`;
+
+  const navigate = useNavigate();
+  const handleBusFeed = () => {navigate("../SubPages/BusFeed.js");};
+  const handleTerminalFeed = () => {navigate("../SubPages/BusFeed.js");};
+
+  
   return (
     <div className="submenu-container">
       <div className="submenu-content">
@@ -23,6 +29,15 @@ function Webcam() {
       </div>
       <img src={BusIcon} className="bus-photo_img" alt="photo_img"/>
       <img src={TerminalIcon} className="terminal-photo_img" alt="photo_img"/>
+
+      <form>
+          <button type="submit" className="login-button" onClick={handleBusFeed}>
+            View Bus Live Feed
+          </button>
+          <button type="submit" className="login-button" onClick={handleTerminalFeed}>
+            View Terminal Live Feed
+          </button>
+        </form>
     </div>
 );
 }
