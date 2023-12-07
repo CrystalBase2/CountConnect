2f4e03e8b2cfca5941ed5f0dd7d18074413a8522
Kurt Zidane Toledo <kurtzidanetoledo@gmail.com>
2023-12-07 23:11:01 +0800

    added firestore database

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -7,7 +8,7 @@ import { getAuth } from "firebase/auth"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCE1Ow7BYHkLCjsfkXxwN1Gn2BP6QmseFs",
+    apiKey: "AIzaSyCE1Ow7BYHkLCjsfwkXxwN1Gn2BP6QmseFs",
     authDomain: "countconnect-1f5f5.firebaseapp.com",
     projectId: "countconnect-1f5f5",
     storageBucket: "countconnect-1f5f5.appspot.com",
@@ -18,4 +19,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+export {
+    auth,
+    db
+}
