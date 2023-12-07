7048f5524fb038140de21937546799afd714ed6f
Kurt Zidane Toledo <kurtzidanetoledo@gmail.com>
2023-12-07 23:59:40 +0800

    updated reset password and firebase api token

--- src/components/Features/auth/ForgotPassword.jsx
+++ src/components/Features/auth/ForgotPassword.jsx
@@ -1,27 +1,45 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import '../../../css/ForgotPassword.css'
+import { auth } from "../../../firebase";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { Alert } from "@mui/material";
+import "../../../css/ForgotPassword.css";
 
 function ForgotPassword() {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(email);
+
+    try {
+      await sendPasswordResetEmail(auth, email);
+      setAlertMessage("Password reset email sent successfully. Please check your inbox.");
+      setShowAlert(true);
+    } catch (error) {
+      console.log(error)
+      setAlertMessage("Error sending password reset email. Please check your email address.");
+      setShowAlert(true);
+    }
   };
 
   const handleLogin = () => {
-    navigate('/');
+    navigate("/");
   };
-
 
   return (
     <div className="forgotpassword-container">
-      <div className="forgotpassword-left">
-
-      </div>
+      <div className="forgotpassword-left"></div>
       <div className="forgotpassword-right">
+
+        {showAlert && (
+          <Alert severity="info" style={{ backgroundColor: '#D5B690', color: 'darkred', width: '50%', margin: '0 auto' }}>
+            <b>Info: </b>{alertMessage}
+          </Alert>
+        )}
+
         <h1 className="forgotpassword-title">FORGOT PASSWORD</h1>
         <br></br>
         <br></br>
@@ -43,18 +61,17 @@ function ForgotPassword() {
           <br></br>
           <br></br>
 
-          <button type="submit" className="forgotpassword-button" onClick={handleLogin}>
+          <button type="submit" className="forgotpassword-button">
             SUBMIT
           </button>
         </form>
 
         <p className="forgotpassword-signup-link">
           Remember your password?{" "}
-          <a href="/" className="forgotpassword-link">
+          <a href="/" className="forgotpassword-link" onClick={handleLogin}>
             Click Here to Login!
           </a>
         </p>
-
       </div>
     </div>
   );
--- src/firebase.jsx
+++ src/firebase.jsx
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCE1Ow7BYHkLCjsfwkXxwN1Gn2BP6QmseFs",
+    apiKey: "AIzaSyCE1Ow7BYHkLCjsfkXxwN1Gn2BP6QmseFs",
     authDomain: "countconnect-1f5f5.firebaseapp.com",
     projectId: "countconnect-1f5f5",
     storageBucket: "countconnect-1f5f5.appspot.com",
@@ -16,6 +16,7 @@ const firebaseConfig = {
     appId: "1:966615620606:web:622de7a47af81d37023eaf",
     measurementId: "G-T9SF968DWD"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
