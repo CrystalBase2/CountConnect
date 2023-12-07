649b61116488715441f3883a9a3696c34a14ef51
CrystalBase2 <crystalbase02@gmail.com>
2023-12-07 14:34:23 +0800

    Modified table title and contents in Bus Driver Information

--- src/components/Submenu/BusDrivers.jsx
+++ src/components/Submenu/BusDrivers.jsx
@@ -16,41 +16,41 @@ function BusDriver() {
           <thead>
             <tr>
               <th>Bus Number</th>
-              <th>Morning</th>
-              <th>Afternoon</th>
-              <th>Evening</th>
+              <th>ID Number</th>
+              <th>Driver's Name</th>
+              <th>Contact Number</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <td>101</td>
-              <td>00</td>
-              <td>00</td>
-              <td>00</td>
+              <td>123456</td>
+              <td>Input Here</td>
+              <td>09*********</td>
             </tr>
             <tr>
               <td>102</td>
-              <td>00</td>
-              <td>00</td>
-              <td>00</td>
+              <td>123457</td>
+              <td>Input Here</td>
+              <td>09*********</td>
             </tr>
             <tr>
               <td>103</td>
-              <td>00</td>
-              <td>00</td>
-              <td>00</td>
+              <td>123458</td>
+              <td>Input Here</td>
+              <td>09*********</td>
             </tr>
             <tr>
               <td>104</td>
-              <td>00</td>
-              <td>00</td>
-              <td>00</td>
+              <td>123459</td>
+              <td>Input Here</td>
+              <td>09*********</td>
             </tr>
             <tr>
               <td>105</td>
-              <td>00</td>
-              <td>00</td>
-              <td>00</td>
+              <td>123450</td>
+              <td>Input Here</td>
+              <td>09*********</td>
             </tr>
           </tbody>
         </table>
