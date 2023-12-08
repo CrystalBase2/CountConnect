ea758975a7a50c86ef53d831f95bf5da8c84dd2b
Kurt Zidane Toledo <kurtzidanetoledo@gmail.com>
2023-12-08 23:58:13 +0800

    updated to use Context

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,95 +1,85 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useNavigate } from "react-router-dom";
-import { createUserWithEmailAndPassword, sendEmailVerification, onAuthStateChanged } from "firebase/auth";
-import { collection, query, where, getDocs, doc, setDoc, updateDoc } from "firebase/firestore";
-import { auth, db } from "../firebase"
-import { Alert } from "@mui/material";
-import '../css/Registration.css'
+import { Link, useNavigate } from 'react-router-dom';
+import { sendEmailVerification} from 'firebase/auth';
+import { collection, query, where, getDocs, doc, setDoc, updateDoc } from 'firebase/firestore';
+import { UserAuth } from './Features/auth/AuthContext';  // Import the UserAuth context
+import { db } from '../firebase';
+import { Alert } from '@mui/material';
+import '../css/Registration.css';
 
 const Registration = () => {
- const navigate = useNavigate();
- const [registering, setRegistering] = useState(false);
- const [email, setEmail] = useState('');
- const [username, setUsername] = useState('');
- const [password, setPassword] = useState('');
- const [confirmPassword, setConfirmPassword] = useState('');
+  const { createUser, user, onAuthStateChanged } = UserAuth(); // Use the UserAuth context
+  const navigate = useNavigate();
+  const [registering, setRegistering] = useState(false);
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [alert, setAlert] = useState({ show: false, severity: 'info', message: '' });
 
- const [alert, setAlert] = useState({ show: false, severity: "info", message: "" });
-
- useEffect(() => {
-  const unsubscribe = onAuthStateChanged(auth, async (user) => {
-    if (user) {
-      if (user.emailVerified) {
-        const userDocRef = doc(db, "users", user.uid);
-        await updateDoc(userDocRef, { emailVerificationSent: false });
-      }
+  useEffect(() => {
+    if (user && user.emailVerified) {
+      const userDocRef = doc(db, 'users', user.uid);
+      updateDoc(userDocRef, { emailVerificationSent: false });
     }
-  });
+  }, [user]);
 
-  return () => unsubscribe();
- }, []);
 
- useEffect(() => {
-  if (auth.currentUser && auth.currentUser.emailVerified) {
-    const userDocRef = doc(db, "users", auth.currentUser.uid);
-    updateDoc(userDocRef, { emailVerificationSent: false });
-  }
- }, [auth.currentUser]);
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      if (!registering) {
+        if (password.length >= 6 && password === confirmPassword) {
+          setRegistering(true);
+          const usersRef = collection(db, 'users');
+          const q = query(usersRef, where('email', '==', email));
+          const querySnapshot = await getDocs(q);
 
- const handleRegister = async (e) => {
-   e.preventDefault();
-   try {
-     if (!registering) {
-       if (password.length >= 6 && password === confirmPassword) {
-         setRegistering(true);
-         const usersRef = collection(db, 'users');
-         const q = query(usersRef, where("email", "==", email));
-         const querySnapshot = await getDocs(q)
+          if (querySnapshot.empty) {
+            const userCredential = await createUser(email, password); // Use createUser from context
+            const newUser = userCredential.user;
 
-         if (querySnapshot.empty) {
-           const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-           const user = userCredential.user;
+            const userDocRef = doc(db, 'users', newUser.uid);
 
-           const userDocRef = doc(db, "users", user.uid);
+            await setDoc(userDocRef, {
+              userId: newUser.uid,
+              username,
+              email: newUser.email,
+              emailVerificationSent: false,
+            });
 
-           await setDoc(userDocRef, {
-             userId: user.uid,
-             username,
-             email: user.email,
-             emailVerificationSent: false,
-           });
+            await sendEmailVerification(newUser);
+            await updateDoc(userDocRef, { emailVerificationSent: true });
 
-           await sendEmailVerification(user);
-           await updateDoc(userDocRef, { emailVerificationSent: true });
+            setAlert({ show: true, severity: 'success', message: 'Email verification sent. Please verify to login.' });
+            setTimeout(() => {
+              setAlert({ show: false, severity: 'info', message: '' });
+              navigate('/');
+            }, 5000); // 5000 milliseconds = 5 seconds
+          } else {
+            const userDoc = querySnapshot.docs[0];
+            const userData = userDoc.data();
 
-           setAlert({ show: true, severity: "success", message: "Email verification sent. Please verify to login." });
-           setTimeout(() => {
-             setAlert({ show: false, severity: "info", message: "" });
-             navigate("/");
-           }, 5000); // 5000 milliseconds = 5 seconds
-         } else {
-           const userDoc = querySnapshot.docs[0];
-           const userData = userDoc.data();
+            if (userData.emailVerificationSent) {
+              setAlert({ show: true, severity: 'info', message: 'Email verification already sent. Please verify to login' });
+            } else {
+              setAlert({ show: true, severity: 'info', message: 'Email already registered and verified. Please use a different email.' });
+            }
+          }
+        } else if (password.length < 6 && password === confirmPassword) {
+          setAlert({ show: true, severity: 'error', message: 'Password should be at least 6 characters.' });
+        } else {
+          setAlert({ show: true, severity: 'error', message: 'Password does not match with confirm password.' });
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setRegistering(false);
+    }
+  };
 
-           if (userData.emailVerificationSent) {
-             setAlert({ show: true, severity: "info", message: "Email verification already sent. Please verify to login" });
-           } else {
-             setAlert({ show: true, severity: "info", message: "Email already registered and verified. Please use a different email." });
-           }
-         }
-       } else if (password.length < 6 && password === confirmPassword) {
-         setAlert({ show: true, severity: "error", message: "Password should be at least 6 characters." });
-       } else {
-         setAlert({ show: true, severity: "error", message: "Password does not match with confirm password." });
-       }
-     }
-   } catch (error) {
-     console.log(error);
-   } finally {
-     setRegistering(false);
-   }
- };
+
 
   return (
     <div className="registration-container">
