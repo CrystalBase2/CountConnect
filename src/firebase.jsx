d07ee3d6341bec53ecc44459a8ea3695a2ff7fe6
Kurt Zidane Toledo <kurtzidanetoledo@gmail.com>
2023-12-08 09:55:46 +0800

    fixed firebase.jsx to allow .env files to be read

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCE1Ow7BYHkLCjsfkXxwN1Gn2BP6QmseFs",
-    authDomain: "countconnect-1f5f5.firebaseapp.com",
-    projectId: "countconnect-1f5f5",
-    storageBucket: "countconnect-1f5f5.appspot.com",
-    messagingSenderId: "966615620606",
-    appId: "1:966615620606:web:622de7a47af81d37023eaf",
-    measurementId: "G-T9SF968DWD"
+    apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 
