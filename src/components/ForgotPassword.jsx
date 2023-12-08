597c42b969452d5c362e045c335be734cbb9a104
Kurt Zidane Toledo <kurtzidanetoledo@gmail.com>
2023-12-09 00:19:38 +0800

    updated forgot password to use context

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -4,24 +4,27 @@ import { auth } from "../firebase";
 import { sendPasswordResetEmail } from "firebase/auth";
 import { Alert } from "@mui/material";
 import "../css/ForgotPassword.css";
+import { UserAuth } from "./Features/auth/AuthContext";
 
 function ForgotPassword() {
   const navigate = useNavigate();
+  const { user, forgotPass } = UserAuth();
   const [email, setEmail] = useState("");
-  const [showAlert, setShowAlert] = useState(false);
-  const [alertMessage, setAlertMessage] = useState("");
+  const [alert, setAlert] = useState({ show: false, severity: 'info', message: '' });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      await sendPasswordResetEmail(auth, email);
-      setAlertMessage("Password reset email sent successfully. Please check your inbox.");
-      setShowAlert(true);
+      await forgotPass(email);
+      setAlert({ show: true, severity: 'success', message: 'Password reset email sent successfully. Please check your inbox.' });
+      setTimeout(() => {
+        setAlert({ show: false, severity: 'info', message: '' });
+        navigate('/');
+      }, 5000);
     } catch (error) {
       console.log(error)
-      setAlertMessage("Error sending password reset email. Please check your email address.");
-      setShowAlert(true);
+      setAlert({ show: true, severity: 'info', message: 'Error sending password reset email. Please check your email address.' });
     }
   };
 
@@ -34,10 +37,11 @@ function ForgotPassword() {
       <div className="forgotpassword-left"></div>
       <div className="forgotpassword-right">
 
-        {showAlert && (
-          <Alert severity="info" style={{ backgroundColor: '#D5B690', color: 'darkred', width: '50%', margin: '0 auto' }}>
-            <b>Info: </b>{alertMessage}
+        {alert.show && (
+          <Alert severity={alert.severity} style={{ backgroundColor: "#D5B690", color: alert.severity === "success" ? "darkgreen" : "darkred", width: "50%", margin: "0 auto" }}>
+            <b>{alert.severity === "success" ? "Success! " : "Error! "}</b>{alert.message}
           </Alert>
+
         )}
 
         <h1 className="forgotpassword-title">FORGOT PASSWORD</h1>
