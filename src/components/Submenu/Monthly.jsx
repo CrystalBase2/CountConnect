62d7be609b2208ff1e372301c4b6c062ebd33359
CrystalBase2 <crystalbase02@gmail.com>
2023-12-26 20:34:26 +0800

    changed monthly table configurations

--- src/components/Submenu/Monthly.jsx
+++ src/components/Submenu/Monthly.jsx
@@ -26,13 +26,27 @@ function Monthly() {
   const [isActive, setIsActive] = useState(false);
   const [selected, setIsSelected] = useState("Choose a Bus Route");
 
+  const [tableData, setTableData] = useState([
+    [" ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " "]
+   ]);
+
+  const routesData = {
+    "Gaisano Mall - Alubijid": [["101", "00", "00", "00", "00", "00"], 
+    ["102", "00", "00", "00", "00", "00"], ["103", "00", "00", "00", "00", "00"], 
+    ["104", "00", "00", "00", "00", "00"],["105", "00", "00", "00", "00", "00"]],
+  };
+
   return (
     <div className="submenu-container">
       <div className="submenu-content">
         <h1 className="submenu-title">REPORTS</h1>
         <div className="date-icon">
           <FaCalendarAlt />
-          <p className="submenu-date"><h7>Today</h7><br />{formattedDate}</p>
+          <p className="submenu-date"><small>Today</small><br />{formattedDate}</p>
         </div>
       </div>
       <span className="submenu-pagesub">Monthly</span>
@@ -53,44 +67,25 @@ function Monthly() {
             {selected}
             <p className={isActive ? "fas fa-caret-up" : "fas fa-caret-down"} />
           </div>
-          <div className="dropdown-content" style={{ display: isActive ? "block" : "none" }}>
-            <div onClick={(e) => {
-              setIsSelected(e.target.textContent);
-              setIsActive(!isActive);
-            }}
-              className="dropdown-item"> Gaisano Mall - Alubijid
-            </div>
-            <div onClick={(e) => {
-              setIsSelected(e.target.textContent);
-              setIsActive(!isActive);
-            }}
-              className="dropdown-item"> Gaisano Mall - Laguindingan
-            </div>
-            <div onClick={(e) => {
-              setIsSelected(e.target.textContent);
-              setIsActive(!isActive);
-            }}
-              className="dropdown-item"> Gaisano Mall - Libertad
-            </div>
-            <div onClick={(e) => {
-              setIsSelected(e.target.textContent);
-              setIsActive(!isActive);
-            }}
-              className="dropdown-item"> Gaisano Mall - Tagoloan
-            </div>
-            <div onClick={(e) => {
-              setIsSelected(e.target.textContent);
-              setIsActive(!isActive);
-            }}
-              className="dropdown-item"> Gaisano Mall - Villanueva
-            </div>
-          </div>
+              <div className="dropdown-content" style={{ display: isActive ? "block" : "none" }}>
+                <div onClick={(e) => {
+                  setIsSelected(e.target.textContent.trim());
+                  setTableData(routesData[e.target.textContent.trim()]);
+                  setIsActive(!isActive);
+                  }}
+                  className="dropdown-item"> Gaisano Mall - Alubijid
+                </div>
+                <div className="dropdown-item-reports"> Gaisano Mall - Laguindingan </div>
+                <div className="dropdown-item-reports"> Gaisano Mall - Libertad </div>
+                <div className="dropdown-item-reports"> Gaisano Mall - Tagoloan </div>
+                <div className="dropdown-item-reports"> Gaisano Mall - Villanueva </div>
+              </div>
         </div>
       </div>
 
       <h2 className="table-title"><b>{selected}</b></h2>
-      <div className="passenger-table-container">
-        <table className="passenger-table">
+      <div className="reports-table-container">
+        <table className="reports-table">
           <thead>
             <tr>
               <th>Bus Number</th>
@@ -102,47 +97,14 @@ function Monthly() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>101</td>
-              <td>00</td>
-              <td>00</td>
-              <td>00</td>
-              <td>00</td>
-              <td>00</td>
-            </tr>
-            <tr>
-              <td>102</td>
-              <td>00</td>
-              <td>00</td>
-              <td>00</td>
-              <td>00</td>
-              <td>00</td>
-            </tr>
-            <tr>
-              <td>103</td>
-              <td>00</td>
-              <td>00</td>
-              <td>00</td>
-              <td>00</td>
-              <td>00</td>
-            </tr>
-            <tr>
-              <td>104</td>
-              <td>00</td>
-              <td>00</td>
-              <td>00</td>
-              <td>00</td>
-              <td>00</td>
-            </tr>
-            <tr>
-              <td>105</td>
-              <td>00</td>
-              <td>00</td>
-              <td>00</td>
-              <td>00</td>
-              <td>00</td>
-            </tr>
-          </tbody>
+                {tableData && tableData.map((row, index) => (
+                <tr key={index}>
+                  {row.map((cell, i) => (
+                    <td key={i}>{cell}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
         </table>
       </div>
 
