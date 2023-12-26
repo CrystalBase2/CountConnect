33b47e1a547231143335defeeb4c72f554e8d827
CrystalBase2 <crystalbase02@gmail.com>
2023-12-26 18:24:49 +0800

    added functionality to dropdown menu and table contents

--- src/components/Submenu/Unit.jsx
+++ src/components/Submenu/Unit.jsx
@@ -26,13 +26,30 @@ function Unit() {
   const [isActive, setIsActive] = useState(false);
   const [selected, setIsSelected] = useState("Choose a Bus Route");
 
+  const [tableData, setTableData] = useState([
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "]
+   ]);
+
+  const routesData = {
+    "Gaisano Mall - Alubijid": [["101", "00", "00"], ["102", "00", "00"], ["103", "00", "00"],, ["104", "00", "00"],["105", "00", "00"]],
+    "Gaisano Mall - Laguindingan": [["201", "00", "00"], ["202", "00", "00"], ["203", "00", "00"],, ["204", "00", "00"],["205", "00", "00"]],
+    "Gaisano Mall - Libertad": [["301", "00", "00"], ["302", "00", "00"], ["303", "00", "00"],, ["304", "00", "00"],["305", "00", "00"]],
+    "Gaisano Mall - Tagoloan": [["401", "00", "00"], ["402", "00", "00"], ["403", "00", "00"],, ["404", "00", "00"],["405", "00", "00"]],
+    "Gaisano Mall - Villanueva": [["501", "00", "00"], ["502", "00", "00"], ["503", "00", "00"],, ["504", "00", "00"],["505", "00", "00"]],
+  };
+   
+
   return (
     <div className="submenu-container">
       <div className="submenu-content">
         <h1 className="submenu-title">Unit Updates</h1>
         <div className="date-icon">
           <FaCalendarAlt />
-          <p className="submenu-date"><h7>Today</h7><br />{formattedDate}</p>
+          <p className="submenu-date"><small>Today</small><br />{formattedDate}</p>
         </div>
       </div>
       <span className="submenu-subtitle">
@@ -52,36 +69,41 @@ function Unit() {
             <p className={isActive ? "fas fa-caret-up" : "fas fa-caret-down"} />
           </div>
           <div className="dropdown-content" style={{ display: isActive ? "block" : "none" }}>
-            <div onClick={(e) => {
-              setIsSelected(e.target.textContent);
-              setIsActive(!isActive);
-            }}
-              className="dropdown-item"> Gaisano Mall - Alubijid
-            </div>
-            <div onClick={(e) => {
-              setIsSelected(e.target.textContent);
-              setIsActive(!isActive);
-            }}
-              className="dropdown-item"> Gaisano Mall - Laguindingan
-            </div>
-            <div onClick={(e) => {
-              setIsSelected(e.target.textContent);
-              setIsActive(!isActive);
-            }}
-              className="dropdown-item"> Gaisano Mall - Libertad
-            </div>
-            <div onClick={(e) => {
-              setIsSelected(e.target.textContent);
-              setIsActive(!isActive);
-            }}
-              className="dropdown-item"> Gaisano Mall - Tagoloan
-            </div>
-            <div onClick={(e) => {
-              setIsSelected(e.target.textContent);
-              setIsActive(!isActive);
-            }}
-              className="dropdown-item"> Gaisano Mall - Villanueva
-            </div>
+                <div onClick={(e) => {
+                  setIsSelected(e.target.textContent.trim());
+                  setTableData(routesData[e.target.textContent.trim()]);
+                  setIsActive(!isActive);
+                  }}
+                  className="dropdown-item"> Gaisano Mall - Alubijid
+                  </div>
+                <div onClick={(e) => {
+                  setIsSelected(e.target.textContent.trim());
+                  setTableData(routesData[e.target.textContent.trim()]);
+                  setIsActive(!isActive);
+                  }}
+                  className="dropdown-item"> Gaisano Mall - Laguindingan
+                </div>
+                <div onClick={(e) => {
+                  setIsSelected(e.target.textContent.trim());
+                  setTableData(routesData[e.target.textContent.trim()]);
+                  setIsActive(!isActive);
+                  }}
+                  className="dropdown-item"> Gaisano Mall - Libertad
+                </div>
+                <div onClick={(e) => {
+                  setIsSelected(e.target.textContent.trim());
+                  setTableData(routesData[e.target.textContent.trim()]);
+                  setIsActive(!isActive);
+                  }}
+                  className="dropdown-item"> Gaisano Mall - Tagoloan
+                </div>
+                <div onClick={(e) => {
+                  setIsSelected(e.target.textContent.trim());
+                  setTableData(routesData[e.target.textContent.trim()]);
+                  setIsActive(!isActive);
+                  }}
+                  className="dropdown-item"> Gaisano Mall - Villanueva
+                </div>
           </div>
         </div>
       </div>
@@ -98,32 +120,14 @@ function Unit() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>101</td>
-              <td>00</td>
-              <td>00</td>
-            </tr>
-            <tr>
-              <td>102</td>
-              <td>00</td>
-              <td>00</td>
-            </tr>
-            <tr>
-              <td>103</td>
-              <td>00</td>
-              <td>00</td>
-            </tr>
-            <tr>
-              <td>104</td>
-              <td>00</td>
-              <td>00</td>
-            </tr>
-            <tr>
-              <td>105</td>
-              <td>00</td>
-              <td>00</td>
-            </tr>
-          </tbody>
+                {tableData && tableData.map((row, index) => (
+                <tr key={index}>
+                  {row.map((cell, i) => (
+                    <td key={i}>{cell}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
         </table>
       </div>
 
